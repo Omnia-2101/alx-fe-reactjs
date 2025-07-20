7f0127169abc742e23ed0dbc0762b69bc5c65c5c
Omnia <omnia.21.nia.21@gmail.com>
2025-07-20 17:52:00 +0300

    Add Counter component using useState

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,16 +1,16 @@
 import Header from './components/Header';
-import UserProfile from './components/UserProfile';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import Counter from './components/Counter';
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <MainContent />
-      <UserProfile name="Omnia Elnaggar" age={21} bio="Front-End Developer with a creative mind and a love for learning.." />
+      <Counter />
       <Footer />
-    </div>
+    </>
   );
 }
 
