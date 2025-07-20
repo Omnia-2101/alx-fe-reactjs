fa62621b5d8109ed7fd5d499c6f4fc8fade948df
Omnia <omnia.21.nia.21@gmail.com>
2025-07-20 17:43:51 +0300

    --

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: '#f0f0f05d', padding: '20px' }}>
+      <p>Welcome..Explore the cities listed below!</p>
+    </main>
+  );
+}
+
+export default MainContent;
