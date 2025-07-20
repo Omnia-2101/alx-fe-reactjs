17a160ac6339d83562e844d47fdf32c5425af269
Omnia <omnia.21.nia.21@gmail.com>
2025-07-20 21:20:15 +0300

    --

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,8 +1,11 @@
+import { useContext } from 'react';
+import UserContext from './UserContext';
 import UserInfo from './UserInfo';
 
 function UserProfile() {
-  return <UserInfo />;
+  const userData = useContext(UserContext); 
+
+  return <UserInfo userData={userData} />;  
 }
 
 export default UserProfile;
-
