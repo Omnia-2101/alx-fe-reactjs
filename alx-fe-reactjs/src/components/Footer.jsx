d987138a2a90c8561d89a8f754123c10882844ce
Omnia <omnia.21.nia.21@gmail.com>
2025-08-02 21:05:42 +0300

    -

--- alx-fe-reactjs/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: '#eee', textAlign: 'center', padding: '10px' }}>
-      <p>&copy; 2025 Omnia Elnaggar</p>
-    </footer>
-  );
-}
-
-export default Footer;
--- alx-fe-reactjs/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header() {
-  return (
-    <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center', padding: '10px' }}>
-      <h1>Hello..</h1>
-    </header>
-  );
-}
-
-export default Header;
