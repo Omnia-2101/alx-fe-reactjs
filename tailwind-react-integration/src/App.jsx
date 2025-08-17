63bad5bcc286364246c53a0cb0d9eb08a732e93b
Omnia <omnia.21.nia.21@gmail.com>
2025-08-17 23:47:07 +0300

    UserProfile

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,11 +1,11 @@
+import UserProfile from "./components/UserProfile";
+
 function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        🚀 Tailwind CSS is working!
-      </h1>
+    <div className="flex items-center justify-center min-h-screen bg-white">
+      <UserProfile />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
